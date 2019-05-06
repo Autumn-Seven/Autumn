9fcdffb603da2f174789b1e5232b2d84b2945f66
seven li <liwy@mingdutech.com>
2019-05-06 17:45:39 +0800

    test

--- src/pages/test/test.vue
+++ src/pages/test/test.vue
@@ -26,39 +26,28 @@
             login(){
 
 
-//                axios({
-//                    method: 'GET',
-//                    url: '/ws/place/v1/search?keyword=%E9%85%92%E5%BA%97&boundary=nearby(39.908491,116.374328,1000)&key=RPNBZ-3EYK3-ZOA3O-3WTNP-6XTLZ-ZDF22',
-//                    data: {}
-//                })
-//                .then(function (response) {
-//                    console.log(response);
-//                })
-//                .catch(function (error) {
-//                    console.log(error);
-//                });
+                axios({
+                    method: 'GET',
+                    url: '/ws/place/v1/search?keyword=%E9%85%92%E5%BA%97&boundary=nearby(39.908491,116.374328,1000)&key=RPNBZ-3EYK3-ZOA3O-3WTNP-6XTLZ-ZDF22',
+                    data: {}
+                })
+                .then(function (response) {
+                    console.log(response);
+                })
 
 
 
+                axios({
+                    method: 'GET',
+                    url: '/WebServices/WeatherWebService.asmx/getSupportCity?byProvinceName=浙江',
+                    data: {}
+                })
+                .then(function (response) {
+                    var x2js = new X2JS();
+                    var jsonObj = x2js.xml2js( response.data );
+                    console.log(jsonObj);
+                });
 
-//                axios({
-//                    method: 'GET',
-//                    url: '/WebServices/WeatherWebService.asmx/getSupportCity?byProvinceName=浙江',
-//                    data: {}
-//                })
-//                .then(function (response) {
-//                    var x2js = new X2JS();
-//                    console.log(x2js);
-//                    console.log(response.data);
-//                    var jsonObj = x2js.xml2js( response.data );
-//                    var jsonObj2 = x2js.xml2dom( response.data );
-//
-//                    console.log(jsonObj);
-//                    console.log(jsonObj2);
-//                })
-//                .catch(function (error) {
-//                    console.log(error);
-//                });
 
 
 
@@ -86,48 +75,40 @@
                         "Content-Type": "text/xml; charset=utf-8"
                     }
                 }).then(function (response) {
-                    console.log(response);
                     var x2js = new X2JS();
 
-                    var jsonObj = x2js.xml2js(response);
                     var jsonObj2 = x2js.xml2js( response.data );
-
-                    console.log(jsonObj);
                     console.log(jsonObj2);
                 })
 
 
 
 
-//                //soap1.2成功
-//
-//                let xmls='<?xml version="1.0" encoding="utf-8"?>\
-//                    <soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">\
-//                    <soap12:Body>\
-//                <getSupportCity xmlns="http://WebXml.com.cn/">\
-//                    <byProvinceName>浙江</byProvinceName>\
-//                    </getSupportCity>\
-//                    </soap12:Body>\
-//                </soap12:Envelope>';
-//
-//
-//                axios({
-//                    method: 'POST',
-//                    url: '/WebServices/WeatherWebService.asmx',
-//                    data:xmls,
-//                    headers:{
-//                        "Content-Type": "application/soap+xml; charset=utf-8"
-//                    }
-//                }).then(function (response) {
-//                    console.log(response);
-//                    var x2js = new X2JS();
-//
-//                    var jsonObj = x2js.xml2js(response);
-//                    var jsonObj2 = x2js.xml2js( response.data );
-//
-//                    console.log(jsonObj);
-//                    console.log(jsonObj2);
-//                })
+                //soap1.2成功
+
+                let xmls2='<?xml version="1.0" encoding="utf-8"?>\
+                    <soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">\
+                    <soap12:Body>\
+                <getSupportCity xmlns="http://WebXml.com.cn/">\
+                    <byProvinceName>浙江</byProvinceName>\
+                    </getSupportCity>\
+                    </soap12:Body>\
+                </soap12:Envelope>';
+
+
+                axios({
+                    method: 'POST',
+                    url: '/WebServices/WeatherWebService.asmx',
+                    data:xmls2,
+                    headers:{
+                        "Content-Type": "application/soap+xml; charset=utf-8"
+                    }
+                }).then(function (response) {
+                    var x2js = new X2JS();
+                    var jsonObj = x2js.xml2js(response);
+                    console.log(jsonObj);
+
+                })
 
 
 
@@ -135,63 +116,19 @@
 
 
 
+                axios.post('/WebServices/WeatherWebService.asmx/getSupportCity', 'byProvinceName=浙江',
+
+                    {
+                        headers:{
+                            'Content-Type': 'application/x-www-form-urlencoded'
+                        }
+                    }
+                 )
+                .then(res=>{
+                    console.log(res);
+                }).catch(err=>{console.log(err)});
 
 
-
-
-
-
-
-
-
-
-
-//
-//                axios.post('/WebServices/WeatherWebService.asmx/getSupportCity',
-//                    {
-//                        byProvinceName:'浙江'
-//                    },
-//
-//                    {
-//                        headers:{
-//                            'Content-Type': 'application/x-www-form-urlencoded'
-//                        }
-//                    }
-//                 )
-//                .then(res=>{
-//                    console.log(res);
-//                }).catch(err=>{console.log(err)});
-
-
-
-
-
-
-//                let self = this;
-//                this.$ajax().post( '/WebServices/WeatherWebService.asmx/getSupportDataSet', {
-//                    type: "POST",   //访问WebService使用Post方式请求
-//                    contentType: "application/json", //WebService 会返回Json类型
-//                    url: "WebService1.asmx/HelloWorld", //调用WebService的地址和方法名称组合 ---- WsURL/方法名
-//                    data: "{}",         //这里是要传递的参数，格式为 data: "{paraName:paraValue}",下面将会看到
-//                    dataType: 'json',
-//                    success: function(result) {     //回调函数，result，返回值
-//                        console.log(result)
-//                    }
-//                }).then(({r}) => {
-//                    console.log(r)
-//
-//                });
-
-//                this.$ajax.post({
-//                    type: "POST",   //访问WebService使用Post方式请求
-//                    contentType: "application/json", //WebService 会返回Json类型
-//                    url: "WebService1.asmx/HelloWorld", //调用WebService的地址和方法名称组合 ---- WsURL/方法名
-//                    data: "{}",         //这里是要传递的参数，格式为 data: "{paraName:paraValue}",下面将会看到
-//                    dataType: 'json',
-//                    success: function(result) {     //回调函数，result，返回值
-//                        console.log(result)
-//                    }
-//                });
             },
         },
     }
